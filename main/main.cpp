632849383a12fcedfcdc5d99147c4a8e8d20cbb2
eighty2fifty1 <huskernavy70@gmail.com>
2020-10-24 17:05:05 +0300

    updates

--- main/main.cpp
+++ main/main.cpp
@@ -1,134 +1,21 @@
+/*
+ * https://twitter.com/wakwak_koba/
+ */
 
-#include "Arduino.h"
 #include "BLEDevice.h"
-#include "FreeRTOS.h"
-#include "sdkconfig.h"
-#include "BLEUUID.h"
-#include <vector>
-#include <queue>
-#include <tuple>
-#include <map>
-#include <exception>
-#include <string>
+#include "Arduino.h"
 
-//////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
-//                REPEATER/CLIENT                   //
-//////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
+std::vector<BaseType_t> PinnedTasks;
 
-//#define NUMBER_OF_DEVICES 3
-
-using namespace std;
-
-std::map<string, BaseType_t> PinnedTasks;
-std::map<string, BLEAdvertisedDevice> FoundDevices; //holds address and device
-typedef queue<tuple<uint16_t, uint8_t, uint8_t>> my_tuple;
-my_tuple dataList;
-static BLEUUID tempServiceUUID("181A");
-static BLEUUID tempCharUUID("2A20");
-//static boolean doConnect = false;
-static boolean connected = false;
-//static boolean doScan = false;
-static BLERemoteCharacteristic *pTempCharacteristic;
-queue<string> disconnectedDevices;
-
-//static BLEAdvertisedDevice *myDevice;
-std::vector<BLEAdvertisedDevice *> devices;
-int counter = 0;
-bool looping = true;
-int pri = 10;
-
-uint8_t _pData[4];
-
-BLEClient *pClient;
-
-//prototypes
-void recvWithStartEndMarkers();
-static void coreBLEClient(BLEClient *);
-
-String dataString;
-string nameString = "iPhone Stand-in";
-
-HardwareSerial MySerial(2);
-const ::byte numChars = 16;
-char receivedChars[numChars];
-char tempChars[numChars];
-bool newData = false;
-
-//status message
-int status[9];
-
-class MyClientCallback : public BLEClientCallbacks
-{
-    void onConnect(BLEClient *pClient)
-    {
-        Serial.print("onConnect to device: ");
-        Serial.println(pClient->getPeerAddress().toString().c_str());
-    }
-
-    void onDisconnect(BLEClient *pClient)
-    {
-        try
-        {
-            connected = false;
-            Serial.print("onDisconnect to: ");
-
-            Serial.println(pClient->getPeerAddress().toString().c_str());
-            //disconnectedDevices.push(pclient->getPeerAddress().toString().c_str());
-            //PinnedTasks.erase(pclient->getPeerAddress().toString().c_str());
-            //delete pClient;     //should call destructor
-            pClient->unregist();
-            log_e("ondisconnect complete");
-            delay(1000);
-            //vTaskDelete(NULL);    //should delete this task
-            //PinnedTasks.push_back(xTaskCreatePinnedToCore([](void *p) { coreBLEClient((BLEClient *)p); }, "coreClient", 4096, pclient, pri++, NULL, tskNO_AFFINITY));
-        }
-        catch (exception &e)
-        {
-            Serial.println(e.what());
-        }
-    }
-};
-
-static void processorStatistics()
-{
-    for (;;)
-    {
-
-        delay(10000);
-        char ptrTaskList[250];
-        vTaskGetRunTimeStats(ptrTaskList);
-        Serial.println(ptrTaskList);
-    }
-}
-
-static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic,
-                           uint8_t *pData,
-                           size_t length,
-                           bool isNotify)
-{
-    
-    Serial.printf("notifyCallback: %s %s handle: %02x value:", pBLERemoteCharacteristic->getRemoteService()->getClient()->getPeerAddress().toString().c_str(), pBLERemoteCharacteristic->getUUID().toString().c_str(), pBLERemoteCharacteristic->getHandle());
+static void notifyCallback(BLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify)  {
+  Serial.printf("notifyCallback: %s %s handle: %02x value:", pRemoteCharacteristic->getRemoteService()->getClient()->getPeerAddress().toString().c_str(), pRemoteCharacteristic->getUUID().toString().c_str(), pRemoteCharacteristic->getHandle());
   for (int i=0; i<length; i++)
     Serial.printf(" %02x", pData[i]);
   Serial.println();
-    /*
-    uint16_t tempData;
-    _pData[1] = *pData;   //temp byte  (bit)?
-    _pData[0] = *++pData; //2nd temp byte
-    _pData[2] = *++pData; //position byte
-    _pData[3] = *++pData; //batt pct byte
-
-    Serial.println(_pData[2]);
-    tempData = (_pData[0] << 8) | _pData[1];
-    dataList.push(tuple<uint16_t, uint8_t, uint8_t>(tempData, _pData[2], _pData[3]));
-    */
 }
 
-static void coreBLEClient(BLEClient *pClient)
-{
-    for(;;) {  
+static void careBLEClient(BLEClient* pClient) {
+  for(;;) {  
     if(pClient->isDisconnected() && pClient->connect()) {
       auto* pRemoteServiceMap = pClient->getServices();
       for (auto itr : *pRemoteServiceMap)  {
@@ -140,242 +27,42 @@ static void coreBLEClient(BLEClient *pClient)
     }
     vTaskDelay(200 / portTICK_PERIOD_MS);
   }
-    /*
-    for (;;)
-    {
-        
-        try
-        {
-            if (pClient->isDisconnected() && pClient->connect())
-            {
-                Serial.print("inside coreBLEClient of device: ");
-                Serial.println(pClient->getPeerAddress().toString().c_str());
-                BLERemoteService *pTempService = pClient->getService(tempServiceUUID);
-                if (pTempService == nullptr)
-                {
-                    Serial.print("Failed to find our temp service UUID: ");
-                    Serial.println(tempServiceUUID.toString().c_str());
-                    pClient->disconnect();
-                }
-                Serial.println(" - Found our temp service");
-                pTempCharacteristic = pTempService->getCharacteristic(tempCharUUID);
-                if (pTempCharacteristic == nullptr)
-                {
-                    Serial.print("Failed to find our characteristic UUID: ");
-                    Serial.println(tempCharUUID.toString().c_str());
-                    pClient->disconnect();
-                }
-                Serial.println(" - Found our characteristic");
-
-                if (pTempCharacteristic->canNotify())
-                {
-                    Serial.println("registering for notifications");
-                    pTempCharacteristic->registerForNotify(notifyCallback);
-                }
-            }
-
-            vTaskDelay(200 / portTICK_PERIOD_MS);
-        }
-        catch (exception &e)
-        {
-            Serial.println(e.what());
-        }
-        
-    }
-    */
 }
-
-void connectSensor(BLEScanResults *sr)
-{
-    for (int i = 0; i < sr->getCount(); i++)
-    {
-        try
-        {
-            Serial.print("Attempting to connect to client ");
-
-            Serial.println(i);
-            auto advertisedDevice = sr->getDevice(i);
-            if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(tempServiceUUID))
-            {
-                FoundDevices.insert(pair<string, BLEAdvertisedDevice>(advertisedDevice.getAddress().toString().c_str(), advertisedDevice));
-                pClient = BLEDevice::createClient(&advertisedDevice);
-                Serial.println(pClient->getPeerAddress().toString().c_str());
-                pClient->setClientCallbacks(new MyClientCallback());
-                ::string taskHandle = pClient->getPeerAddress().toString().c_str();
-                if (pClient->regist())
-                {
-                    PinnedTasks.insert(pair<string, BaseType_t>(taskHandle, xTaskCreatePinnedToCore([](void *p) { coreBLEClient((BLEClient *)p); }, "coreClient", 4096, pClient, pri++, NULL, tskNO_AFFINITY)));
-                }
-                Serial.print("connected to index ");
-                Serial.println(i);
-                delay(1000);
-            }
-        }
-
-        catch (exception &e)
-        {
-            Serial.print("exception caught: ");
-            Serial.println(e.what());
-        }
-    }
-}
-
-void reconnectSensor(BLEClient *pC)
-{
-    pC->setClientCallbacks(new MyClientCallback());
-    ::string taskHandle = pC->getPeerAddress().toString().c_str();
-    if (pC->regist())
-    {
-        PinnedTasks.insert(pair<string, BaseType_t>(taskHandle, xTaskCreatePinnedToCore([](void *p) { coreBLEClient((BLEClient *)p); }, "coreClient", 4096, pC, pri++, NULL, tskNO_AFFINITY)));
-    }
-}
-
-void serialRecv()
-{
-    Serial.println("receive function called");
-    recvWithStartEndMarkers();
-    if (newData == true)
-    {
-        strcpy(tempChars, receivedChars);
-        // this temporary copy is necessary to protect the original data
-        //   because strtok() used in parseData() replaces the commas with \0
-        //parseData();
-        //showParsedData();
-        newData = false;
-    }
-}
-
-void recvWithStartEndMarkers()
-{
-    static boolean recvInProgress = false;
-    static ::byte ndx = 0;
-    char startMarker = '<';
-    char endMarker = '>';
-    char rc;
-    //Serial.println("other recv fucntion called");
-    while (MySerial.available() > 0 && newData == false)
-    {
-        //Serial.println("data found");
-        rc = MySerial.read();
-        //Serial.print(rc);
-
-        if (recvInProgress == true)
-        {
-            if (rc != endMarker)
-            {
-                receivedChars[ndx] = rc;
-                ndx++;
-                if (ndx >= numChars)
-                {
-                    ndx = numChars - 1;
-                }
-            }
-            else
-            {
-                receivedChars[ndx] = '\0'; // terminate the string
-                recvInProgress = false;
-                ndx = 0;
-                newData = true;
-            }
-        }
-
-        else if (rc == startMarker)
-        {
-            recvInProgress = true;
-        }
-    }
-}
-
-void parseData()
-{ // split the data into its parts
-
-    char *strtokIndx; // this is used by strtok() as an index
-
-    strtokIndx = strtok(tempChars, ","); // get the first part - the string
-    status[0] = atoi(strtokIndx);        // copy it to messageFromPC
-
-    for (int i = 1; i < 10; i++)
-    {
-        strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-        status[i] = atoi(strtokIndx);   // convert this part to an integer
-    }
-}
-
-void showParsedData()
-{
-    for (int t = 0; t < 9; t++)
-    {
-        Serial.print(status[t]);
-        Serial.print(", ");
-    }
-    Serial.println();
-}
-
-
-
-extern "C"
-{
+extern "C"{
     void app_main(void);
 }
 
-void app_main(void)
-{
-    Serial.begin(115200);
-    Serial.println("Starting Arduino BLE Client application...");
-    BLEDevice::init("");
+void app_main(void) {
+  Serial.begin(115200);
 
-    BLEScan *pBLEScan = BLEDevice::getScan();
+  BLEDevice::init("");
+  auto* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setActiveScan(true);
 
-    pBLEScan->setActiveScan(true);
-    auto pScanResults = pBLEScan->start(5);
-    //pBLEScan->stop();
-    Serial.println("Connectable Devices");
+  Serial.println("wait 10 secs..");
+  auto pScanResults = pBLEScan->start(10);
 
-    connectSensor(&pScanResults);
-
-    //PinnedTasks.insert(pair<string, BaseType_t>("stats", xTaskCreatePinnedToCore([](void *p) {processorStatistics();}, "stats", 4096, NULL, 5, NULL, tskNO_AFFINITY)));
-
-    //BLEDevice::getScan()->stop();
-
-    MySerial.begin(9600, SERIAL_8N1);
-
-    for (;;)
-    {
-        /*
-        if (MySerial.available() > 0)
-        {
-            serialRecv();
-            //serialRelay();
-        }
-*/
-        /*
-       while(!disconnectedClients.empty()){
-           Serial.println("attempting reconnect");
-           reconnectSensor(disconnectedClients.front());
-           disconnectedClients.pop();
-       }
-       */
-        /*
-        while (!dataList.empty())
-        {
-            dataString = "<";
-            dataString += get<1>(dataList.front());
-            dataString += "i";
-            dataString += get<0>(dataList.front());
-            dataString += "i";
-            dataString += get<2>(dataList.front());
-            dataString += ">";
-            MySerial.println(dataString);
-            //Serial.println(dataString);
-            //Serial.println(PinnedTasks.size());
-            dataList.pop();
-            delay(10);
-            
-        }
-        */
-        delay(50);
-        if (PinnedTasks.empty()){
-            ESP.restart();
-        }
+  for (int i = 0; i < pScanResults.getCount(); i++) {
+    auto advertisedDevice = pScanResults.getDevice(i);
+    if (advertisedDevice.haveServiceUUID())  {
+      Serial.print("Found Device ");
+      Serial.print(advertisedDevice.toString().c_str());
+      auto pClient = BLEDevice::createClient(&advertisedDevice);
+      if(pClient->regist()) {
+        PinnedTasks.push_back(xTaskCreatePinnedToCore([](void *p) {careBLEClient((BLEClient*)p);}
+          , "careBLEClient", 4096, pClient, 10, NULL, tskNO_AFFINITY)
+        );
+        Serial.println(" registered");
+      } else 
+        Serial.println();
     }
+  }
+
+  if(!PinnedTasks.size())
+    ESP.restart();
+
+
+for (;;) {
+	delay(10);
+}
 }
---
+++ main/main2.cpp
@@ -0,0 +1,357 @@
+#include "Arduino.h"
+#include "BLEDevice.h"
+#include "FreeRTOS.h"
+#include "sdkconfig.h"
+#include "BLEUUID.h"
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <map>
+#include <exception>
+#include <string>
+#include <array>
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//                REPEATER/CLIENT                   //
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+//#define NUMBER_OF_DEVICES 3
+
+using namespace std;
+
+std::vector<BaseType_t> PinnedTasks;
+typedef queue<tuple<uint16_t, uint8_t, uint8_t>> my_tuple;
+my_tuple dataList;
+static BLEUUID tempServiceUUID("181A");
+static BLEUUID tempCharUUID("2A20");
+static BLERemoteCharacteristic *pTempCharacteristic;
+
+int pri = 10;
+uint8_t _pData[4];
+
+BLEClient *pClient;
+
+//prototypes
+void serialRecv();
+void recvWithStartEndMarkers();
+static void coreBLEClient(BLEClient *);
+void scan();
+void scanTask();
+void showParsedData();
+void resetDevice();
+void sendSensorReport();
+void forceScan();
+void sensorSleep();
+
+String dataString;
+//string nameString = "iPhone Stand-in";
+
+HardwareSerial MySerial(2);
+const ::byte numChars = 16;
+char receivedChars[numChars];
+char tempChars[numChars];
+bool newData = false;
+
+//status message.  all are bool except number of sensors
+array<int, 9> status; //<reset, set number of sensors, send mac and rssi, force scan, sleep>
+
+int sensors = 4; //default for 2 axle.  should be stored in memory
+
+class MyClientCallback : public BLEClientCallbacks
+{
+    void onConnect(BLEClient *pClient)
+    {
+        Serial.print("onConnect to device: ");
+        Serial.println(pClient->getPeerAddress().toString().c_str());
+    }
+
+    void onDisconnect(BLEClient *pClient)
+    {
+        Serial.print("onDisconnect to: ");
+        Serial.println(pClient->getPeerAddress().toString().c_str());
+        //disconnectedDevices.push(pClient);
+        //pClient->disconnect();
+        //delete pClient;
+    }
+};
+
+static void processorStatistics()
+{
+    for (;;)
+    {
+        delay(10000);
+        char ptrTaskList[250];
+        vTaskGetRunTimeStats(ptrTaskList);
+        Serial.println(ptrTaskList);
+    }
+}
+
+static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic,
+                           uint8_t *pData,
+                           size_t length,
+                           bool isNotify)
+{
+    uint16_t tempData;
+    _pData[1] = *pData;   //temp byte  (bit)?
+    _pData[0] = *++pData; //2nd temp byte
+    _pData[2] = *++pData; //position byte
+    _pData[3] = *++pData; //batt pct byte
+    tempData = (_pData[0] << 8) | _pData[1];
+    dataList.push(tuple<uint16_t, uint8_t, uint8_t>(tempData, _pData[2], _pData[3]));
+}
+
+static void coreBLEClient(BLEClient *pClient)
+{
+    for (;;)
+    {
+        if (pClient->isDisconnected() && pClient->connect())
+        {
+            auto *pRemoteServiceMap = pClient->getServices();
+            for (auto itr : *pRemoteServiceMap)
+            {
+                auto *pCharacteristicMap = itr.second->getCharacteristicsByHandle();
+                for (auto itr : *pCharacteristicMap)
+                    if (itr.second->canNotify())
+                        itr.second->registerForNotify(notifyCallback);
+            }
+        }
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+}
+
+void connectSensor(BLEScanResults *sr)
+{
+    for (int i = 0; i < sr->getCount(); i++)
+    {
+        auto advertisedDevice = sr->getDevice(i);
+        if (advertisedDevice.isAdvertisingService(tempServiceUUID))
+        {
+            pClient = BLEDevice::createClient(&advertisedDevice);
+            Serial.println(pClient->getPeerAddress().toString().c_str());
+            pClient->setClientCallbacks(new MyClientCallback());
+
+            if (pClient->regist())
+            {
+                PinnedTasks.push_back(xTaskCreatePinnedToCore([](void *p) { coreBLEClient((BLEClient *)p); }, pClient->getPeerAddress().toString().c_str(), 4096, pClient, pri++, NULL, tskNO_AFFINITY));
+            }
+            delay(1000);
+        }
+    }
+}
+
+extern "C"
+{
+    void app_main(void);
+}
+
+void scan()
+{
+    BLEScan *pBLEScan = BLEDevice::getScan();
+    pBLEScan->setActiveScan(true);
+    BLEScanResults pScanResults = pBLEScan->start(5);
+    //pBLEScan->stop();
+    Serial.println("Connectable Devices");
+
+    //print list of found devices for debugging
+    for (int i = 0; i < pScanResults.getCount(); i++)
+    {
+        Serial.println(pScanResults.getDevice(i).getAddress().toString().c_str());
+    }
+    connectSensor(&pScanResults);
+}
+
+void app_main(void)
+{
+    Serial.begin(115200);
+    MySerial.begin(9600, SERIAL_8N1);
+
+    Serial.println("Starting Arduino BLE Client application...");
+    BLEDevice::init("");
+
+    PinnedTasks.push_back(xTaskCreatePinnedToCore([](void *p) { processorStatistics(); }, "stats", 4096, NULL, 5, NULL, tskNO_AFFINITY));
+    scan();
+
+    for (;;)
+    {
+        if (MySerial.available() > 0)
+        {
+            serialRecv();
+        }
+        while (!dataList.empty())
+        {
+            dataString = "<";
+            dataString += get<1>(dataList.front());
+            dataString += "i";
+            dataString += get<0>(dataList.front());
+            dataString += "i";
+            dataString += get<2>(dataList.front());
+            dataString += ">";
+            MySerial.println(dataString);
+            Serial.println(dataString);
+            //Serial.println(PinnedTasks.size());
+            dataList.pop();
+            delay(10);
+        }
+
+        delay(10);
+        if (PinnedTasks.empty())
+        {
+            ESP.restart();
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////
+//                                                      //
+//                  Serial Functions                    //
+//    These functions are for handling incoming serial  //
+//    messages to control various system functions      //
+//////////////////////////////////////////////////////////
+void serialRecv()
+{
+    Serial.println("receive function called");
+    recvWithStartEndMarkers();
+    if (newData == true)
+    {
+        strcpy(tempChars, receivedChars);
+        // this temporary copy is necessary to protect the original data
+        //   because strtok() used in parseData() replaces the commas with \0
+        //parseData();
+        //showParsedData();
+        newData = false;
+    }
+}
+
+void recvWithStartEndMarkers()
+{
+    static boolean recvInProgress = false;
+    static ::byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+    //Serial.println("other recv fucntion called");
+    while (MySerial.available() > 0 && newData == false)
+    {
+        //Serial.println("data found");
+        rc = MySerial.read();
+        //Serial.print(rc);
+
+        if (recvInProgress == true)
+        {
+            if (rc != endMarker)
+            {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars)
+                {
+                    ndx = numChars - 1;
+                }
+            }
+            else
+            {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker)
+        {
+            recvInProgress = true;
+        }
+    }
+}
+
+void parseData()
+{ // split the data into its parts
+
+    char *strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(tempChars, ","); // get the first part - the string
+    status[0] = atoi(strtokIndx);        // copy it to messageFromPC
+
+    for (int i = 1; i < status.size(); i++)
+    {
+        strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+        status[i] = atoi(strtokIndx);   // convert this part to an integer
+    }
+
+    showParsedData(); //for debugging
+
+    if (status[0])
+    {
+        resetDevice();
+    }
+
+    else
+    {
+        if (status[1] > 0)
+        {
+            if (sensors != status[1])
+            {
+                sensors = status[1];
+            }
+        }
+
+        if (status[2])
+        {
+            sendSensorReport();
+        }
+
+        if (status[3])
+        {
+            forceScan();
+        }
+
+        if (status[4])
+        {
+            sensorSleep();
+        }
+    }
+}
+
+void showParsedData()
+{
+    for (int t = 0; t < status.size(); t++)
+    {
+        Serial.print(status[t]);
+        Serial.print(", ");
+    }
+    Serial.println();
+}
+
+void resetDevice()
+{
+    ESP.restart();
+}
+
+void forceScan()
+{
+}
+
+void forceReconnect(string addr)
+{
+}
+
+//saves expected number of sensors.  needs to save it in memory
+void setNumSensors(int s)
+{
+    sensors = s;
+}
+
+void sendSensorReport()
+{
+}
+
+//acknowledges the reset message and resets value in server
+//may not be necessary?
+void sendResetConfirm()
+{
+}
+
+void sensorSleep()
+{
+}
